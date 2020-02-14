ed0e496c4e25d4a2733dc73f7dedf85a6d296628
codechris1 <christianmunozrosas@yahoo.com.mx>
2020-02-14 15:29:45 -0500

    moving everything to utils and separating the scripts

---
+++ lib/move_by_dates.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import datetime
+import time
+from utils import console, load_json_file, set_dict_entry, move_by_dates, save_output
+
+args = {
+    "path": "/Cargas de cámara",
+    "to_path": "/Cargas de cámara/2011",
+    "from_date": "20110101", # Format YYYYMMDD
+    "to_date": "20111231", # Format YYYYMMDD
+    "recursive": True,
+    "limit": 2000,
+    "wait_time": 5
+}
+
+def main():
+    move_by_dates(args)
+    save_output(console, "log/console.json")
+
+if __name__ == '__main__':
+    main()
