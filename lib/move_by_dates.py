b0da716440f8d329a62dbca2b7bf9c0a84675968
codechris1 <christianmunozrosas@yahoo.com.mx>
2020-02-16 01:11:15 -0500

    defining default parameters on move by dates

--- lib/move_by_dates.py
+++ lib/move_by_dates.py
@@ -5,14 +5,14 @@ import time
 from utils import console, load_json_file, set_dict_entry, move_by_dates, save_output, move_by_dates_101
 
 args = {
-    "path": "/Cargas de cámara/2011",
-    "to_path": "/Cargas de cámara/2011",
-    "from_date": "20110101", # Format YYYYMMDD
-    "to_date": "20111201", # Format YYYYMMDD
-    "recursive": True,
+    "path": "/Cargas de cámara/2017",
+    "to_path": "/Photos/your folder/iPhone",
+    "from_date": "20170508", # Format YYYYMMDD
+    "to_date": "20170512", # Format YYYYMMDD
+    "recursive": False,
     "limit": 2000,
     "wait_time": 90, # Only required by move_by_dates function, move_by_dates_101 doesn't need it
-    "move_criteria": 2  # 1 = by date , the previous criteria will be used | 2 by origin folder (it will move all files from origin folder)
+    "move_criteria": 1  # 1 = by date , the previous criteria will be used | 2 by origin folder (it will move all files from origin folder)
 }
 
 def main():
